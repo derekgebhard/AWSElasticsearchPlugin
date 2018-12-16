4a8fcc1b796c3119fd072ecc7e6b1704728b767a
Gebhard <derekgeb@6c96cfdb9fd5.ant.amazon.com>
2018-12-15 22:56:37 -0500

    Initial setup of plugin

---
+++ src/index.js
@@ -0,0 +1,52 @@
+/**
+ * @module serverless-elasticsearch
+ *
+ * @see {@link https://serverless.com/framework/docs/providers/aws/guide/plugins/}
+ *
+ * @requires 'bluebird'
+ * */
+
+
+
+ /**
+  * @class ESPlugin
+  * @classdesc Serverless plugin for managing AWS ES Clusters
+  * */
+ class ESPlugin {
+   /**
+    * @description Serverless Elasticsearch
+    * @constructor
+    *
+    * @param {!Object} serverless - Serverless object
+    * @param {!Object} options - Serverless options
+    * */
+   constructor (serverless, options) {
+     /** Serverless variables */
+     this.serverless = serverless;
+     this.options = options;
+
+     this.provider = this.serverless.getProvider('aws');
+     this.stage = this.provider.getStage();
+
+     this.hooks = {
+       'after:package:initialize': this.afterPackageInitialize.bind(this),
+       //'after:package:createDeploymentArtifacts': this.afterCreateDeploymentArtifacts.bind(this),
+       //'after:deploy:deploy': this.afterDeployFunctions.bind(this),
+     }
+   }
+
+   /**
+   * @description After package initialize hook. Create warmer function and add it to the service.
+   *
+   * @fulfil {} — Warm up set
+   * @reject {Error} Warm up error
+   *
+   * @return {(boolean|Promise)}
+   * */
+  afterPackageInitialize () {
+    return this.serverless.cli.log('Elasticsearch: no clusters to create');
+  }
+}
+
+/** Export WarmUP class */
+module.exports = ESPlugin;
